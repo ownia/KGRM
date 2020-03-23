b2af4a5835b0fcaa6691dcbdb1e925ced1e3ecc2
ownia <451686892@qq.com>
2020-03-23 16:35:31 +0800

    add kdnode-graph

--- kg_spider/kg_spider/tool/txt_clean.py
+++ kg_spider/kg_spider/tool/txt_clean.py
@@ -1,7 +1,7 @@
 def addentity(inputfile):
     fp = open(inputfile, "r", encoding="utf-8")
     lines = fp.readlines()
-    str = "家电品牌,包括,"
+    str = "快递物流品牌,包括,"
     with open("output.txt", "a+", encoding="utf-8")as f:
         for line in lines:
             line_new = line.replace('（', '(').replace('）', ')')
@@ -20,6 +20,15 @@ def append(inputfile):
     fp.close()
 
 
+def kdbind(inputfile):
+    fp = open(inputfile, encoding="utf-8")
+    lines = fp.readlines()
+    with open("output.txt", "a+", encoding="utf-8")as f:
+        for line in lines:
+            line_new = line.replace('{"name": "', '').replace('"},', '')
+            f.write(line_new)
+
+
 if __name__ == '__main__':
-    inputfile = "1.txt"
+    inputfile = "2.txt"
     addentity(inputfile)
