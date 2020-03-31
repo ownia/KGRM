02fda0b2d9306491db7ada110a748a24cf5920c2
ownia <451686892@qq.com>
2020-03-31 13:54:01 +0800

    add echarts.js

--- kg_web/app.py
+++ kg_web/app.py
@@ -8,14 +8,9 @@ def index():
     return render_template('index.html')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
