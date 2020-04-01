3686ae98f1ff4cffd690a447f9f3c1143f2240da
ownia <451686892@qq.com>
2020-04-01 13:29:30 +0800

    transfer DataFrame to table item

--- kg_web/app.py
+++ kg_web/app.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template, request, redirect, abort
+from flask import Flask, render_template, request, redirect, abort, jsonify
+from py2neo import Graph
+from collections import OrderedDict
+import pandas as pd
 
 app = Flask(__name__)
+graph = Graph()
 
 
-@app.route('/')
+def buildNodes(nodeRecord):
+    data = {"id": str(nodeRecord.n._id), "label": next(iter(nodeRecord.n.labels))}
+    data.update(nodeRecord.n.properties)
+    return {"data": data}
+
+
+def buildEdges(relationRecord):
+    data = {"source": str(relationRecord.r.start_node._id),
+            "target": str(relationRecord.r.end_node._id),
+            "relationship": relationRecord.r.rel.type}
+    return {"data": data}
+
+
+@app.route('/', methods=("GET", "POST"))
 def index():
-    return render_template('index.html')
+    pd.set_option('display.float_format', lambda x: '%.4f' % x)
+    df = pd.DataFrame(graph.run(
+        'MATCH (:class{title:"快递物流品牌"})-[r:RELATION {type :"包括"  }]->(n) RETURN n.title, n.registered ORDER BY '
+        'n.registered').to_table())
+    df.drop(0, axis=0, inplace=True)
+    return render_template('index.html',
+                           tables=[df.to_html(classes='table table-dark table-striped')],
+                           titles=df.columns.values)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/graph')
+def get_graph():
+    nodes = map(buildNodes, graph.run('MATCH (:class{title:"快递物流品牌"})-[r:RELATION {type :"包括"  }]->(n) RETURN n'))
+    edges = map(buildEdges, graph.run('MATCH (:class{title:"快递物流品牌"})-[r:RELATION {type :"包括"  }]->(n) RETURN r'))
+    data = list({"nodes": nodes, "edges": edges})
+    return jsonify(elements=data)
 
 
 if __name__ == '__main__':
