9e9c04290f8dafac21fab002917e983de15cc673
ownia <451686892@qq.com>
2020-04-05 17:54:27 +0800

    add imgs

--- kg_spider/kg_spider/tool/data_clean.py
+++ kg_spider/kg_spider/tool/data_clean.py
@@ -2,6 +2,7 @@ import json
 import csv
 import codecs
 import sys
+import os
 
 
 # 删除冗余数据
@@ -15,9 +16,23 @@ def delete_whitespace(jsonName, txtName):
     wf.close()
 
 
+def price_clean(filedir):
+    filenames = os.listdir(filedir)
+    f = open('result.txt', 'w', encoding='utf-8')
+    for filename in filenames:
+        filepath = filedir + '/' + filename
+        # 遍历单个文件，读取行数
+        for line in open(filepath, encoding='utf-8'):
+            f.writelines(line)
+        f.write('\n')
+    f.close()
+
+
 if __name__ == '__main__':
     name = 'whirlpool'
     jsonName = name + '.json'
     txtName = name + '.txt'
-    delete_whitespace(jsonName, txtName)
+    # delete_whitespace(jsonName, txtName)
+    filedir = './data'
+    price_clean(filedir)
     print("deleted.")
