6f2883e816d0a73766c1c6d0beb47708541effd0
ownia <451686892@qq.com>
2020-04-19 10:28:06 +0800

    add sim-coe

--- kg_web/app.py
+++ kg_web/app.py
@@ -5,6 +5,8 @@ import pandas as pd
 import json
 import hanlp
 import re
+import numpy as np
+from neo4j import GraphDatabase, basic_auth, kerberos_auth, custom_auth, TRUST_ALL_CERTIFICATES
 
 app = Flask(__name__)
 graph = Graph()
@@ -164,6 +166,82 @@ def ner():
         except:
             print('post error.')
     return render_template('ner.html', data=data, text=text, ner_output=ner_output, page=page)
+
+
+def Jaccard(terms_model, reference):
+    grams_reference = set(reference)
+    grams_model = set(terms_model)
+    temp = 0
+    for i in grams_reference:
+        if i in grams_model:
+            temp = temp + 1
+    dis = len(grams_model) + len(grams_reference) - temp
+    jaccard_coefficient = float(temp / dis)
+    return jaccard_coefficient
+
+
+def dice_coefficient(a, b):
+    """dice coefficient 2nt/na + nb."""
+    a_bigrams = set(a)
+    b_bigrams = set(b)
+    overlap = len(a_bigrams & b_bigrams)
+    return overlap * 2.0 / (len(a_bigrams) + len(b_bigrams))
+
+
+def edit_distance(word1, word2):
+    len1 = len(word1)
+    len2 = len(word2)
+    dp = np.zeros((len1 + 1, len2 + 1))
+    for i in range(len1 + 1):
+        dp[i][0] = i
+    for j in range(len2 + 1):
+        dp[0][j] = j
+    for i in range(1, len1 + 1):
+        for j in range(1, len2 + 1):
+            delta = 0 if word1[i - 1] == word2[j - 1] else 1
+    dp[i][j] = min(dp[i - 1][j - 1] + delta, min(dp[i - 1][j] + 1, dp[i][j - 1] + 1))
+    return dp[len1][len2]
+
+
+def take_third(elem):
+    return elem[2]
+
+
+@app.route('/similarity_coefficient')
+def sim_coe():
+    data = count()
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+    session = driver.session()
+    data = session.run("MATCH (m)-[r]->(n) RETURN m.title, r.relation, n.title LIMIT 100")
+    # print(data)
+    blists = []
+    out_j = []
+    out_d = []
+    for d in data:
+        bs = str(d[0])
+        blists.append(bs)
+    for i in range(len(blists)):
+        for j in range(0, i):
+            a = blists[i]
+            b = blists[j]
+            td_j = Jaccard(a, b)
+            td_d = dice_coefficient(a, b)
+            std = edit_distance(a, b) / max(len(a), len(b))
+            fy = 1 - std
+            avg_j = (td_j + fy) / 2
+            avg_d = (td_d + fy) / 2
+            if avg_j < 1:
+                # print(blists[i], blists[j])
+                # print('avg_sim: ', avg_j)
+                out_j.append(blists[i] + " " + blists[j] + " " + str(avg_j))
+            if avg_d < 1:
+                out_d.append(blists[i] + " " + blists[j] + " " + str(avg_d))
+    list_j = list(set(out_j))
+    list_d = list(set(out_d))
+    list_j.sort(key=take_third)
+    list_d.sort(key=take_third)
+    return render_template('similarity_coefficient.html', data=data, text_j=list_j, text_d=list_d)
 
 
 @app.errorhandler(404)
--- kg_web/test.py
+++ kg_web/test.py
@@ -4,6 +4,8 @@ import json
 import re
 import chardet
 from demjson import decode
+import numpy as np
+from neo4j import GraphDatabase, basic_auth, kerberos_auth, custom_auth, TRUST_ALL_CERTIFICATES
 
 
 def build_nodes(node_record):
@@ -87,5 +89,66 @@ class Solution:
         return data
 
 
+def Jaccard(terms_model, reference):
+    grams_reference = set(reference)
+    grams_model = set(terms_model)
+    temp = 0
+    for i in grams_reference:
+        if i in grams_model:
+            temp = temp + 1
+    dis = len(grams_model) + len(grams_reference) - temp
+    jaccard_coefficient = float(temp / dis)
+    return jaccard_coefficient
+
+
+def dice_coefficient(a, b):
+    """dice coefficient 2nt/na + nb."""
+    a_bigrams = set(a)
+    b_bigrams = set(b)
+    overlap = len(a_bigrams & b_bigrams)
+    return overlap * 2.0 / (len(a_bigrams) + len(b_bigrams))
+
+
+def edit_distance(word1, word2):
+    len1 = len(word1)
+    len2 = len(word2)
+    dp = np.zeros((len1 + 1, len2 + 1))
+    for i in range(len1 + 1):
+        dp[i][0] = i
+    for j in range(len2 + 1):
+        dp[0][j] = j
+    for i in range(1, len1 + 1):
+        for j in range(1, len2 + 1):
+            delta = 0 if word1[i - 1] == word2[j - 1] else 1
+    dp[i][j] = min(dp[i - 1][j - 1] + delta, min(dp[i - 1][j] + 1, dp[i][j - 1] + 1))
+    return dp[len1][len2]
+
+
+def print_title_of(tx, name):
+    for record in tx.run("MATCH (a:class)-[]->(f) "
+                         "WHERE a.name = {name} "
+                         "RETURN f.name", name=name):
+        print(record["f.name"])
+
+
 if __name__ == '__main__':
-    print("ok.")
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+    session = driver.session()
+    data = session.run("MATCH (m)-[r]->(n) RETURN m.title, r.relation, n.title LIMIT 100")
+    # print(data)
+    blists = []
+    for d in data:
+        bs = str(d[0])
+        blists.append(bs)
+    for i in range(len(blists)):
+        for j in range(0, i):
+            a = blists[i]
+            b = blists[j]
+            td = Jaccard(a, b)
+            std = edit_distance(a, b) / max(len(a), len(b))
+            fy = 1 - std
+            avg = (td + fy) / 2
+            if avg < 1:
+                print(blists[i], blists[j])
+                print('avg_sim: ', avg)
